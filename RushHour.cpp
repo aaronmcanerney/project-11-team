e1511c6f30773752587b79987a001ecbd0565f60
aaron mcanerney <aaron.mac016@yahoo.com>
2017-12-04 03:32:03 +0000

    forgot to delete old comments

--- RushHour.cpp
+++ RushHour.cpp
@@ -567,72 +567,7 @@ void solve(int& numMoves, Vehicle cars[], Board& board, int& best, const int& nu
 
 
     }
-    /*
-    def breadth_first_search(problem):
-
-  # a FIFO open_set
-  open_set = Queue()
-  # an empty set to maintain visited nodes
-  closed_set = set()
-  # a dictionary to maintain meta information (used for path formation)
-  meta = dict()  # key -> (parent state, action to reach child)
-
-  # initialize
-  start = problem.get_start_state()
-  meta[start] = (None, None)
-  open_set.enqueue(start)
-
-  while not open_set.is_empty():
-
-    parent_state = open_set.dequeue()
-
-    if problem.is_goal(parent_state):
-          return construct_path(parent_state, meta)
-
-    for (child_state, action) in problem.get_successors(parent_state):
-
-      if child_state in closed_set:
-        continue
-
-      if child_state not in open_set:
-        meta[child_state] = (parent_state, action)
-        open_set.enqueue(child_state)
-
-    closed_set.add(parent_state)
-
-*/
-
-
     
-    
-    
-    /*if(isComplete(cars[0], board) || numMoves > best){
-        if(numMoves < best){
-            best = numMoves;
-            result = true;
-            return;
-        }
-        else{
-            return;
-        }
-        //return;
-    }
-    else{ 
-        for(int i = 0; i < numCars; i++){
-            if(moveForward(cars[i], board)){
-                solve(numMoves + 1, cars, board, best, numCars, result);
-                moveBackward(cars[i], board);
-            }
-            
-            if(moveBackward(cars[i], board)){
-                solve(numMoves +  1, cars, board, best, numCars, result);
-                moveForward(cars[i], board);
-            }
-
-        }
-    return;
-    }
-    */
 }
 
 
